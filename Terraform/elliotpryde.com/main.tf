493e912017dd987a1a68caca0d3d9a83ba8fca35
Elliot Pryde <elliotpryde@hotmail.co.uk>
2020-08-11 19:55:40 +0100

    Use protonmail domain module for Terraform 0.13

--- Terraform/elliotpryde.com/main.tf
+++ Terraform/elliotpryde.com/main.tf
@@ -1,6 +1,6 @@
 module "protonmail-domain" {
   source  = "app.terraform.io/elliotpryde/protonmail-domain/aws"
-  version = "1.0.0"
+  version = "1.1.0"
 
   zone_id           = aws_route53_zone.elliotpryde-com.zone_id
   verification_data = var.protonmail_elliotpryde_com_verification_string
