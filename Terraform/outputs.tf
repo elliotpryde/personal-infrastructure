b80cf742bc318237fa4de85897c2d1314aaf12a7
Elliot Pryde <elliot.pryde@elliotpryde.com>
2021-03-31 13:21:24 +0100

    add aws zone_id output for elliotpryde.com

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -8,3 +8,8 @@ output "nas-traefik-user-access-key-secret" {
   description = "The secret for the AWS user access key used by Traefik to complete DNS challenges."
   sensitive = true
 }
+
+output "elliotpryde-com-zone-id" {
+  value = module.elliotpryde-com.elliotpryde-com-zone-id
+  description = "The AWS hosted zone ID for elliotpryde.com."
+}
