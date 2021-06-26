028db2005190b74483e982e6ebb6858ea18bd569
Elliot Pryde <elliot.pryde@elliotpryde.com>
2021-06-26 22:42:45 +0100

    specify required minimum provider versions

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -2,12 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.47.0"
     }
     azurerm = {
       source = "hashicorp/azurerm"
+      version = "~> 2.65.0"
     }
     time = {
       source = "hashicorp/time"
+      version = "~> 0.7.1"
     }
   }
   required_version = ">= 0.13"
