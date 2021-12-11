10aafb373fb5e283a69a32fee10d6c3ab401321d
Elliot Pryde <elliot.pryde@elliotpryde.com>
2021-12-11 18:05:15 +0000

    bump azurerm provider version

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 2.65.0"
+      version = "~> 2.89.0"
     }
     time = {
       source = "hashicorp/time"
